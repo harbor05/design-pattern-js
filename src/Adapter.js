cc8c7a81764d0e7924b8c7cc8f51303d6ad61428
Joowon Jang <joowon.jang@hotmail.com>
2022-06-10 15:14:54 +0900

    Adapter updated

--- src/Adapter.js
+++ src/Adapter.js
@@ -1,5 +1,63 @@
-/**
- * Adapter Pattern
- * @description
- */
-class Adapter {}
+/******* Adapter Pattern(Wrapper pattern) *******/
+// 22.06.10 기준 미완성
+// 인터페이스가 일치하지 않는 컴포넌트를 함께 사용할 수 있게 도와준다.
+// 새로운 컴포넌트가 기존에 만들어진 컴포넌트를 통합하거나 리팩토링하여 사용한다.
+/******* End Adapter Pattern(Wrapper pattern) *******/
+
+// Old interface
+class Onboarding {
+  // return Onboarding message
+  request(name, age) {
+    return `Member ${name} is ${age} years old`;
+  }
+}
+
+// Updated interface
+class AdvancedOnboarding {
+  /**
+   * 학교 등록
+   * @param {String} college 학교 이름
+   */
+  setCollege(college) {
+    this.college = college;
+  }
+
+  /**
+   * 도시 등록
+   * @param {String} city 출신 도시 이름
+   */
+  setCity(city) {
+    this.city = city;
+  }
+
+  /**
+   * @param {Number} siblings 형제, 자매 인원
+   */
+  setSiblings(siblings) {
+    this.siblings = siblings;
+  }
+
+  // return New Onboarding message
+  setRegistration(name, age) {
+    return `Member ${name} is ${age} years old and has ${this.siblings} siblings, from ${this.city} also graduated ${this.college}`;
+  }
+}
+
+// Adapter
+class OnboardingAdapter {
+  onboarding = new AdvancedOnboarding(); // New Onboarding Class
+  request(name, age, college, city, siblings) {
+    this.onboarding.setCity(city);
+    this.onboarding.setCollege(college);
+    this.onboarding.setSiblings(siblings);
+    return this.onboarding.setRegistration(name, age);
+  }
+}
+
+let onboarding = new Onboarding();
+let member = onboarding.request("bill", 25);
+console.log("Old Message:", member);
+
+let _onboarding = new OnboardingAdapter();
+let _member = _onboarding.request("bill", 25, "MIT", "Boston", 0);
+console.log("New Message:", _member);
