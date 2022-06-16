841b69142dc79d7724b2ab35e20d86afaa971f55
Joowon Jang <joowon.jang@hotmail.com>
2022-06-16 13:30:20 +0900

    Observer Pattern, Readme updated

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [Meta Information](#Meta-Information)
 - [Project Structure](#Project-structure)
 - [Getting Start](#Getting-Start)
-- [Postmortem](#Postmortem)
+- [Issue](#Issue)
 
 </br>
 
@@ -19,7 +19,28 @@
 
 ```bash
 ├── src/
+│   ├── Adapter.js
+│   ├── Composite.js
+│   ├── Observer.js
+│   ├── Prototype.js
+│   ├── Singleton.js
+│   ├── StrategyMethod.js
+│   ├── TemplateMethod.js
+│   └──
 └──
 ```
 
 </br>
+
+### Descriptions (in progress)
+
+- **Singleton Pattern**  
+  인스턴스를 하나 생성한 뒤 이미 생성한 인스터스가 있을 경우 기존의 인스턴스를 사용한다.
+- **Observer Pattern(Pub/Sub)**  
+  이벤트 핸들러가 이벤트를 발생 시키고 이벤트에 대한 인수를 전달하여 Pub/Sub 형태로 작동한다. (e.g. 클릭 이벤트가 발생한 곳의 좌표)
+
+</br>
+
+### Issue
+
+-
--- src/Observer.js
+++ src/Observer.js
@@ -1,4 +1,51 @@
-/**
- * Observer Pattern
- */
-class Observer {}
+// Click Event
+class Click {
+  handlers = [];
+
+  /**
+   * subscribe
+   * @param {Function} fn Event Handler
+   */
+  subscribe(fn) {
+    this.handlers.push(fn);
+  }
+
+  /**
+   * unsubscribe
+   * @param {Funciton} fn Event Handler
+   */
+  unSubscribe(fn) {
+    this.handlers = this.handlers.filter((item) => {
+      if (item !== fn) {
+        return item;
+      }
+    });
+  }
+
+  /**
+   * fire
+   * @param {String} arg Argument for "this"
+   * @return this 와 arguments 를 매개로 호출된 함수의 반환값
+   */
+  fire(arg) {
+    let isServer = typeof window !== "undefined";
+    if (isServer) return;
+
+    this.handlers.forEach((fn) => {
+      fn.call(this, arg); // 구독 한 event에 arg를 바인딩
+    });
+  }
+}
+
+const clickHandlers = (item) => {
+  console.log("fired +", item);
+};
+
+let click = new Click();
+
+click.subscribe(clickHandlers);
+click.fire("First Event");
+click.unSubscribe(clickHandlers);
+click.fire("Seconde Event");
+click.subscribe(clickHandlers);
+click.fire("Third Event");
