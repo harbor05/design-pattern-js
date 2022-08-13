a15d0b2c4c0eefb3b5a328202d37915fa12e90f1
Joowon Jang <joowon.jang@hotmail.com>
2022-08-13 14:06:45 +0900

    TemplateMethod update

--- src/TemplateMethod.js
+++ src/TemplateMethod.js
@@ -1,4 +1,54 @@
+/*! *****************************************************************************
+- Template Method Pattern(22.08.13 - 작성 중)
+***************************************************************************** */
+
+// 추상화 된 객체
+const dataStore = {
+  process() {
+    this.connect();
+    this.select();
+    this.disconnect();
+    return true;
+  },
+};
+
 /**
- * Template Method Pattern
+ * @class Inherit
+ *
+ * @param {Object} proto - dataStore 추상화되어 있는 객체
+ *
+ * @return new
  */
-class TemplateMethod {}
+class Inherit {
+  constructor(proto) {
+    this._proto = proto;
+  }
+
+  get proto() {
+    return this._proto.process();
+  }
+
+  set proto(props) {
+    this._proto.process() = props;
+  }
+  
+}
+
+let mySql = new Inherit(dataStore); // 구체적인 클래스를 작성하기 위한 mySQL, 이후 Method를 할당
+
+// implement methods
+mySql._proto.connect = () => {
+  console.log("MySQL: connect");
+};
+
+mySql._proto.select = () => {
+  console.log("MySQL: select");
+};
+
+mySql._proto.disconnect = () => {
+  console.log("MySQL: disconnect");
+};
+
+// run process method
+mySql._proto.process();
+
